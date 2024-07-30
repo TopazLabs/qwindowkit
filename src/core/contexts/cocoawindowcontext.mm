5138338d5b1c506ddb71878a0ab43c435eca6ac2
Adam Mains <adam.mains@topazlabs.com>
2024-07-30 12:16:26 -0500

    Viewsize

--- src/core/contexts/cocoawindowcontext.mm
+++ src/core/contexts/cocoawindowcontext.mm
@@ -234,7 +234,7 @@ namespace QWK {
             // do the necessary transformations
             center.ry() = titlebarHeight - center.y();
 
-            qInfo() << "[DBG] Updating system button rect:" << titlebarHeight << viewSize << center << width << height;
+            qInfo() << "[DBG] Updating system button rect:" << titlebarHeight << viewSize.width << viewSize.height << center << width << height;
 
             // Mid button
             NSPoint centerOrigin = {
